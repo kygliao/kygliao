e92c9e83995067b8d74766bf46fb8b8436fa6213
Kaiying Liao <kygliao@gmail.com>
2020-06-22 00:20:06 -0400

    Updates

---
+++ precache-manifest.04a676314dc95b6eb8706e60c649bcb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4bc6b2227ad67d52c1d82db2bcfacc",
+    "url": "/kygliao/index.html"
+  },
+  {
+    "revision": "5c792d7b89f77d0974c3",
+    "url": "/kygliao/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "153b7049d0cc0b59b1fd",
+    "url": "/kygliao/static/js/2.4ea2d27e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kygliao/static/js/2.4ea2d27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c792d7b89f77d0974c3",
+    "url": "/kygliao/static/js/main.3bf02333.chunk.js"
+  },
+  {
+    "revision": "9c6067970fc7c89cb09e",
+    "url": "/kygliao/static/js/runtime-main.a8fd87b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-page/precache-manifest.9fb5e3bc8c448823be40ca7f43a3b62f.js"
+  "/kygliao/precache-manifest.04a676314dc95b6eb8706e60c649bcb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kygliao/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
